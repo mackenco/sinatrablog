68ded0d1b84a28cc7278def349d063601aa2921b
Colin MacKenzie <cmackenzie@mediamath.com>
2013-12-06 17:06:05 -0500

    Added validations

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require "sinatra/activerecord"
 set :database, "sqlite3:///blog.db"
 
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :body, presence: true
 end
 
 get "/" do
